326c98169ee5921a5887367b9b0603367d4233ca
Carlos Espinoza <74637956+c3spinoza@users.noreply.github.com>
2021-04-05 17:30:34 -0500

    Added Comments

--- VU/vu.js
+++ VU/vu.js
@@ -1,9 +1,10 @@
-function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
+//Opened Menu
+function openNav() { 
+  document.getElementById("mySidebar").style.width = "250px";  
   document.getElementById("main").style.marginLeft = "250px";
 }
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
+//Closed Menu
+function closeNav() { 
+  document.getElementById("mySidebar").style.width = "0";                
   document.getElementById("main").style.marginLeft = "0";
 }
