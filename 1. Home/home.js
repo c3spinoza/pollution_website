492247ede164fa6ebe99a02a04fcbd534c321dc3
Carlos Espinoza <74637956+c3spinoza@users.noreply.github.com>
2021-04-21 13:03:36 -0500

    Google Maps integrations

--- 1. Home/home.js
+++ 1. Home/home.js
@@ -1,1 +1,17 @@
+window.addEventListener('load', function() {
+   init();
+});
+
+function initMap() {
+  var location = {lat: -25.363,lng: 131.044};
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: location,
+  });
+  new google.maps.Marker({
+    position: location,
+    map,
+    title: "Hello World!",
+  });
+}
 
