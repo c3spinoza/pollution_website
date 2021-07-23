666aadd0404f333cb2a5ffe404f1a8a7278cbe55
Carlos Espinoza <74637956+c3spinoza@users.noreply.github.com>
2021-07-23 11:17:56 -0500

    Added New Map JS

--- 1. Home/home.js
+++ 1. Home/home.js
@@ -1,12 +1,12 @@
-function initMap() {
-  var location = {lat: -25.363,lng: 131.044};
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: location,
-  });
-  new google.maps.Marker({
-    position: location,
-    map,
-    title: "Hello World!",
-  });
-} 
+//Map
+var mymap = L.map('map').setView([51.505, -0.09], 13);
+//Map's Source
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGFsbGlhYmxlLXNvY2lldG9sb2d5IiwiYSI6ImNrcmF2ODlkcjNpY3YycnJ4OTR6eWVhNmEifQ.wIwj4TNBBVeScPzezR5I0g', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox/">Mapbox</a>',
+  //Map Additional Attributes
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoidGFsbGlhYmxlLXNvY2lldG9sb2d5IiwiYSI6ImNrcmF2ODlkcjNpY3YycnJ4OTR6eWVhNmEifQ.wIwj4TNBBVeScPzezR5I0g'
+}).addTo(mymap);
